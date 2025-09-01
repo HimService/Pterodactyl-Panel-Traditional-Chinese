2eeaa04ff26aaf1b8377f20e160c2db8a9c20e97
him40706 <wmeili174@gmail.com>
2025-09-01 20:38:32 +0800

    翻譯v1.11.11內容

--- v1.11.11/resources/views/admin/nests/new.blade.php
+++ v1.11.11/resources/views/admin/nests/new.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.admin')
 
 @section('title')
-    New Nest
+    新核心區
 @endsection
 
 @section('content-header')
-    <h1>New Nest<small>Configure a new nest to deploy to all nodes.</small></h1>
+    <h1>新核心區<small>設定一個要部署到所有節點的新核心區。</small></h1>
     <ol class="breadcrumb">
-        <li><a href="{{ route('admin.index') }}">Admin</a></li>
-        <li><a href="{{ route('admin.nests') }}">Nests</a></li>
-        <li class="active">New</li>
+        <li><a href="{{ route('admin.index') }}">管理</a></li>
+        <li><a href="{{ route('admin.nests') }}">核心區</a></li>
+        <li class="active">新增</li>
     </ol>
 @endsection
 
@@ -19,18 +19,18 @@
         <div class="col-md-12">
             <div class="box">
                 <div class="box-header with-border">
-                    <h3 class="box-title">New Nest</h3>
+                    <h3 class="box-title">新核心區</h3>
                 </div>
                 <div class="box-body">
                     <div class="form-group">
-                        <label class="control-label">Name</label>
+                        <label class="control-label">名稱</label>
                         <div>
                             <input type="text" name="name" class="form-control" value="{{ old('name') }}" />
-                            <p class="text-muted"><small>This should be a descriptive category name that encompasses all of the eggs within the nest.</small></p>
+                            <p class="text-muted"><small>這應該是一個描述性的類別名稱，涵蓋核心區中的所有核心。</small></p>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="control-label">Description</label>
+                        <label class="control-label">描述</label>
                         <div>
                             <textarea name="description" class="form-control" rows="6">{{ old('description') }}</textarea>
                         </div>
@@ -38,7 +38,7 @@
                 </div>
                 <div class="box-footer">
                     {!! csrf_field() !!}
-                    <button type="submit" class="btn btn-primary pull-right">Save</button>
+                    <button type="submit" class="btn btn-primary pull-right">儲存</button>
                 </div>
             </div>
         </div>
