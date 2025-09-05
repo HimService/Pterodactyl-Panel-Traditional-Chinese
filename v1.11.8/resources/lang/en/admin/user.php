450c9cc932a878e9ed5de9d6aa11af2b74970205
him40706 <wmeili174@gmail.com>
2025-09-06 00:06:15 +0800

    翻譯v1.11.8內容

--- v1.11.8/resources/lang/en/admin/user.php
+++ v1.11.8/resources/lang/en/admin/user.php
@@ -2,10 +2,10 @@
 
 return [
     'exceptions' => [
-        'user_has_servers' => 'Cannot delete a user with active servers attached to their account. Please delete their servers before continuing.',
+        'user_has_servers' => '無法刪除帳戶中擁有伺服器的使用者。請先刪除其伺服器再繼續。',
     ],
     'notices' => [
-        'account_created' => 'Account has been created successfully.',
-        'account_updated' => 'Account has been successfully updated.',
+        'account_created' => '已成功建立帳戶。',
+        'account_updated' => '已成功更新帳戶。',
     ],
 ];
