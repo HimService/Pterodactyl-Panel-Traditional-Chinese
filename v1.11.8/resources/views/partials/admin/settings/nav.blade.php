65e33e9857ac6744b27a585c261f732d2ac42b9f
him40706 <wmeili174@gmail.com>
2025-09-06 12:48:06 +0800

    翻譯v1.11.8內容

--- v1.11.8/resources/views/partials/admin/settings/nav.blade.php
+++ v1.11.8/resources/views/partials/admin/settings/nav.blade.php
@@ -6,9 +6,9 @@
         <div class="col-xs-12">
             <div class="nav-tabs-custom nav-tabs-floating">
                 <ul class="nav nav-tabs">
-                    <li @if($activeTab === 'basic')class="active"@endif><a href="{{ route('admin.settings') }}">General</a></li>
-                    <li @if($activeTab === 'mail')class="active"@endif><a href="{{ route('admin.settings.mail') }}">Mail</a></li>
-                    <li @if($activeTab === 'advanced')class="active"@endif><a href="{{ route('admin.settings.advanced') }}">Advanced</a></li>
+                    <li @if($activeTab === 'basic')class="active"@endif><a href="{{ route('admin.settings') }}">一般設定</a></li>
+                    <li @if($activeTab === 'mail')class="active"@endif><a href="{{ route('admin.settings.mail') }}">郵件設定</a></li>
+                    <li @if($activeTab === 'advanced')class="active"@endif><a href="{{ route('admin.settings.advanced') }}">進階設定</a></li>
                 </ul>
             </div>
         </div>
