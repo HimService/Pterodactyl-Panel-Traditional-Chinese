699c8e8d22ea803ef0d0894a39d7c10ae9e112eb
him40706 <wmeili174@gmail.com>
2025-09-06 18:11:13 +0800

    翻譯v3.7.3內容

--- v3.7.3/resources/views/admin/nests/new.blade.php
+++ v3.7.3/resources/views/admin/nests/new.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.admin')
 
 @section('title')
-    New Nest
+    新巢
 @endsection
 
 @section('content-header')
-    <h1>New Nest<small>Configure a new nest to deploy to all nodes.</small></h1>
+    <h1>新巢<small>設定要部署到所有節點的新巢。</small></h1>
     <ol class="breadcrumb">
-        <li><a href="{{ route('admin.index') }}">Admin</a></li>
-        <li><a href="{{ route('admin.nests') }}">Nests</a></li>
-        <li class="active">New</li>
+        <li><a href="{{ route('admin.index') }}">管理</a></li>
+        <li><a href="{{ route('admin.nests') }}">巢</a></li>
+        <li class="active">新增</li>
     </ol>
 @endsection
 
@@ -19,36 +19,36 @@
         <div class="col-md-12">
             <div class="box">
                 <div class="box-header with-border">
-                    <h3 class="box-title">New Nest</h3>
+                    <h3 class="box-title">新巢</h3>
                 </div>
                 <div class="box-body">
                     <div class="form-group">
-                        <label class="control-label">Name</label>
+                        <label class="control-label">名稱</label>
                         <div>
                             <input type="text" name="name" class="form-control" value="{{ old('name') }}" />
-                            <p class="text-muted"><small>This should be a descriptive category name that encompasses all of the eggs within the nest.</small></p>
+                            <p class="text-muted"><small>這應該是一個描述性的類別名稱，其中包含巢中的所有環境。</small></p>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="control-label">Description</label>
+                        <label class="control-label">說明</label>
                         <div>
                             <textarea name="description" class="form-control" rows="6">{{ old('description') }}</textarea>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="control-label">Nest Visibility</label>
+                        <label class="control-label">巢可見性</label>
                         <div>
                             <select name="private" class="form-control">
-                                <option selected value="0">Public</option>
-                                <option value="1">Private</option>
+                                <option selected value="0">公開</option>
+                                <option value="1">私人</option>
                             </select>
-                            <p class="text-muted"><small>Determines whether users can deploy to this nest.</small></p>
+                            <p class="text-muted"><small>確定使用者是否可以部署到此巢。</small></p>
                         </div>
                     </div>
                 </div>
                 <div class="box-footer">
                     {!! csrf_field() !!}
-                    <button type="submit" class="btn btn-primary pull-right">Save</button>
+                    <button type="submit" class="btn btn-primary pull-right">儲存</button>
                 </div>
             </div>
         </div>
