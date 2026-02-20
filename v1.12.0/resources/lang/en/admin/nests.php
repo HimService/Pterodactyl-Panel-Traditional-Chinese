a98997248bf59e82a41505f7b8511161ae8e7fbd
him40706 <wmeili174@gmail.com>
2026-02-20 22:28:58 +0800

    翻譯1.12.0

--- v1.12.0/resources/lang/en/admin/nests.php
+++ v1.12.0/resources/lang/en/admin/nests.php
@@ -2,25 +2,25 @@
 
 return [
     'notices' => [
-        'created' => 'A new nest, :name, has been successfully created.',
-        'deleted' => 'Successfully deleted the requested nest from the Panel.',
-        'updated' => 'Successfully updated the nest configuration options.',
+        'created' => '已成功建立新的 Nest :name。',
+        'deleted' => '已成功從面板中刪除請求的 Nest。',
+        'updated' => '已成功更新 Nest 配置選項。',
     ],
     'eggs' => [
         'notices' => [
-            'imported' => 'Successfully imported this Egg and its associated variables.',
-            'updated_via_import' => 'This Egg has been updated using the file provided.',
-            'deleted' => 'Successfully deleted the requested egg from the Panel.',
-            'updated' => 'Egg configuration has been updated successfully.',
-            'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
-            'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
+            'imported' => '已成功匯入此 Egg 及其相關變數。',
+            'updated_via_import' => '此 Egg 已使用提供的檔案進行更新。',
+            'deleted' => '已成功從面板中刪除請求的 Egg。',
+            'updated' => 'Egg 配置已成功更新。',
+            'script_updated' => 'Egg 安裝指令碼已更新，並將在安裝伺服器時執行。',
+            'egg_created' => '已成功產下新的 Egg。您需要重新啟動任何正在執行的 daemon 以套用此新 Egg。',
         ],
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
-            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
-            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
+            'variable_deleted' => '變數 ":variable" 已被刪除，重建後將不再適用於伺服器。',
+            'variable_updated' => '變數 ":variable" 已更新。您需要重建任何使用此變數的伺服器以套用變更。',
+            'variable_created' => '已成功建立新變數並指派給此 Egg。',
         ],
     ],
 ];
